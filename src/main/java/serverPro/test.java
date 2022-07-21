6f2935cef24b1559aadd5e0bb61902aa743e03b6
webyiyeon <yiyeon79@gmail.com>
2022-07-21 11:27:39 +0900

    1st commit

---
+++ src/main/java/serverPro/test.java
@@ -0,0 +1,9 @@
+package serverPro;
+
+import java.io.IOException;
+
+public class test {
+	public static void main(String[] args) throws IOException{
+		System.out.println("220721_서버프로그램구현 : 박이연");
+	}
+}
